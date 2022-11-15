4e2bbb6ab465115b4f8921faf444121f58971820
saidabdul80 <saidabdulsalam5@gmail.com>
2022-11-15 17:27:02 +0100

    fix updates

---
+++ public/build/assets/Success.ac2dce2a.js
@@ -0,0 +1,1 @@
+import{o as e,c as s}from"./app.8042de50.js";const c={style:{width:"300px"},class:"shadow alert alert-success"},l={__name:"Success",props:{},setup(t){return(o,a)=>(e(),s("div",c," Completed Successfully "))}};export{l as default};
